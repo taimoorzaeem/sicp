0aaeb6274d2f19ef2a7b48809c62d4507f381de6
Taimoor Zaeem <mtaimoorzaeem@gmail.com>
2023-02-03 12:31:07 +0500

    Add Ex 2.30

--- 2.2.scm
+++ 2.2.scm
@@ -112,3 +112,19 @@
   (cond ((null? x) '())
         ((not (pair? x)) (list x))
         (else (append (fringe (car x)) (fringe (cdr x))))))
+
+
+;; Ex 2.30
+;; ==============
+(define (square-tree tree)
+  (cond ((null? tree) '())
+        ((not (pair? tree)) (* tree tree))
+        (else (cons (square-tree (car tree))
+                    (square-tree (cdr tree))))))
+
+(define (square-tree-map tree)
+  (map (lambda (sub-tree)
+          (if (pair? sub-tree)
+              (square-tree-map sub-tree)
+              (* sub-tree sub-tree)))
+       tree))
