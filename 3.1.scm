e06d93b918f1872be6730e9fb9c1ce6ba73f9223
Taimoor Zaeem <mtaimoorzaeem@gmail.com>
2023-08-13 15:45:37 +0500

    Add Ex 3.2

--- 3.1.scm
+++ 3.1.scm
@@ -5,3 +5,14 @@
   (lambda (sum)
     (begin (set! init-sum (+ init-sum sum))
            init-sum)))
+
+;; Ex 3.2
+;; ==================
+
+(define (make-monitored f)
+  (let ((calls 0))
+    (lambda (input)
+      (if (eq? input 'how-many-calls)
+          calls
+          (begin (set! calls (+ calls 1))
+                 (f input))))))
