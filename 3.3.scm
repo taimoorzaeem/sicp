15037aabf0803f71f07e5f376c3b5abd1fa536a8
M. Taimoor Zaeem <mtaimoorzaeem@gmail.com>
2024-07-20 14:55:45 +0500

    Add Ex 3.24 and Ex 3.27

--- 3.3.scm
+++ 3.3.scm
@@ -55,3 +55,48 @@
              (else 
               (error "Undefined operation: QUEUE" m)))) 
      dispatch))
+
+;; Ex 3.24
+;; ================
+(define (make-table-with-key-proc same-key?)
+	(let ((local-table (list '*table*)))
+    (define (assoc key records)
+      (cond ((null? records) false)
+            ((same-key? key (caar records)) (car records))
+            (else (assoc key (cdr records)))))
+		(define (lookup key-1 key-2)
+			(let ((subtable
+							(assoc key-1 (cdr local-table))))
+				(if subtable
+						(let ((record
+									 (assoc key-2 (cdr subtable))))
+							(if record (cdr record) false))
+						false)))
+		(define (insert! key-1 key-2 value)
+			(let ((subtable
+							(assoc key-1 (cdr local-table))))
+				(if subtable
+						(let ((record
+						       (assoc key-2 (cdr subtable))))
+			        (if record
+				          (set-cdr! record value)
+				          (set-cdr! subtable
+			                      (cons (cons key-2 value)
+			                            (cdr subtable)))))
+			      (set-cdr! local-table
+			                (cons (list key-1 (cons key-2 value))
+		                        (cdr local-table)))))
+		  'ok)
+		(define (dispatch m)
+      (cond ((eq? m 'lookup-proc) lookup)
+            ((eq? m 'insert-proc!) insert!)
+            (else (error "Unknown operation: TABLE" m))))
+    dispatch))
+
+
+;; Ex 3.27
+;; ==================
+
+;; No, the scheme would not have worked if we have simply defined the function
+;; as (memoize fib). This is because fib does not recursively call to memoize
+;; the result.
