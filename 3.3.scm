10558d72075c2b3fb58b0f93f87d1f07b7d73957
M. Taimoor Zaeem <mtaimoorzaeem@gmail.com>
2024-07-21 18:07:45 +0500

    Add Ex 3.28 and 3.29

--- 3.3.scm
+++ 3.3.scm
@@ -100,3 +100,67 @@
 ;; No, the scheme would not have worked if we have simply defined the function
 ;; as (memoize fib). This is because fib does not recursively call to memoize
 ;; the result.
+
+(define (logical-not s)
+	(cond ((= s 0) 1)
+				((= s 1) 0)
+				(else (error "Invalid signal" s))))
+
+(define (inverter input output)
+	(define (invert-input)
+		(let ((new-value (logical-not (get-signal input))))
+			(after-delay inverter-delay
+									(lambda () (set-signal! output new-value)))))
+	(add-action! input invert-input) 'ok)
+
+(define (logical-and a b)
+	(cond ((and (= a 0) (= b 0)) 0)
+				((and (= a 0) (= b 1)) 0)
+				((and (= a 1) (= b 0)) 0)
+				((and (= a 1) (= b 1)) 1)
+				(else (error "Invalid signals" a b))))
+
+(define (and-gate a1 a2 output)
+	(define (and-action-procedure)
+		(let ((new-value
+					(logical-and (get-signal a1) (get-signal a2))))
+		 (after-delay
+			and-gate-delay
+			(lambda () (set-signal! output new-value)))))
+	(add-action! a1 and-action-procedure)
+	(add-action! a2 and-action-procedure)
+	'ok)
+
+;; Ex 3.28
+;; ==============
+
+(define (logical-or a b)
+	(cond ((and (= a 0) (= b 0)) 0)
+				((and (= a 0) (= b 1)) 1)
+				((and (= a 1) (= b 0)) 1)
+				((and (= a 1) (= b 1)) 1)
+				(else (error "Invalid signals" a b))))
+
+(define (or-gate a1 a2 output)
+	(define (or-action-procedure)
+		(let ((new-value
+					(logical-or (get-signal a1) (get-signal a2))))
+		 (after-delay or-gate-delay
+									(lambda () (set-signal! output new-value)))))
+	(add-action! a1 or-action-procedure)
+	(add-action! a2 or-action-procedure)
+	'ok)
+
+;; Ex 3.29
+;; ==============
+
+(define (or-gate-other a1 a2 output)
+	(define (or-action-procedure)
+		(let ((new-value
+					(logical-not (logical-and	(logical-not (get-signal a1))
+															      (logical-not (get-signal a2))))))
+			(after-delay (+ (* inverter-delay 2) and-gate-delay)
+									 (lambda () (set-signal! output new-value)))))
+	(add-action! a1 or-action-procedure)
+	(add-action! a2 or-action-procedure)
+	'ok)
