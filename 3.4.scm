5390e0ef4bff6a6d5f5e4da960b2750a59be33f8
M. Taimoor Zaeem <mtaimoorzaeem@gmail.com>
2024-07-24 03:56:39 +0500

    Add Ex 3.47a

--- 3.4.scm
+++ 3.4.scm
@@ -16,3 +16,51 @@
 ;; Possiblities remain after serializing
 ;;
 ;; P1 = 100 or P2 = 1000
+
+(define (clear! cell) (set-car! cell false))
+
+(define (test-and-set! cell)
+	(if (car cell) true (begin (set-car! cell true) false)))
+
+(define (make-mutex)
+	(let ((cell (list false)))
+		(define (the-mutex m)
+			(cond ((eq? m 'acquire)
+						 (if (test-and-set! cell)
+								 (the-mutex 'acquire)))
+						((eq? m 'release) (clear! cell))))
+		the-mutex))
+
+
+(define (make-serializer)
+	(let ((mutex (make-mutex)))
+		(lambda (p)
+			(define (serialized-p . args)
+				(mutex 'acquire)
+				(let ((val (apply p args)))
+					(mutex 'release)
+					val))
+			serialized-p)))
+
+
+;; Ex 3.47
+;; ================
+
+;; Implement semaphore using mutex
+(define (make-semaphore)
+	(let ((mutex (make-mutex))
+				(c 0))
+		(define (acquire)
+			(mutex 'acquire)
+			(cond ((< c n) (set! c (+ c 1)) (mutex 'release))
+						(else (mutex 'release) (acquire))))
+		(define (release)
+			(mutex 'acquire)
+      (if (<= 1 c) (set! c (- c 1))
+									 (error "This semaphore is not acquired yet"))
+      (mutex 'release))
+    (define (dispatch m)
+      (cond [(eq? m 'acquire) (acquire)]
+            [(eq? m 'release) (release)]
+            [else (error "Unknown message sent to a semaphore" m)]))
+    dispatch))
