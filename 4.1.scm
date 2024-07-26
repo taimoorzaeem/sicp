d71e9cd94e7a582258080462d26bc0922c3c2e9b
M. Taimoor Zaeem <mtaimoorzaeem@gmail.com>
2024-07-26 20:10:30 +0500

    Add definitions in eval

--- 4.1.scm
+++ 4.1.scm
@@ -18,3 +18,51 @@
 								(list-of-values	(operands exp) env)))
 				(else
 					(error "Unknown expression type: EVAL" exp))))
+
+
+(define	(apply procedure arguments)
+	(cond ((primitive-procedure? procedure)
+				 (apply-primitive-procedure procedure arguments))
+				((compound-procedure? procedure)
+				 (eval-sequence
+					  (procedure-body procedure)
+						(extend-environment
+							(procedure-body procedure)
+						  arguments
+							(procedure-environment procedure))))
+				(else (error "Unknown procedure type: APPLY" procedure))))
+
+
+(define (list-of-values exps env)
+	(if (no-operands? exps)
+			'()
+			(cons (eval (first-operand exps) env)
+						(list-of-values (rest-operands exps) env))))
+
+
+(define (eval-if exp env)
+	(if (true? (eval (if-predicate exp) env))
+			(eval (if-consequent exp) env)
+			(eval (if-alternative exp) env)))
+
+
+(define (eval-sequence exps env)
+	(cond ((last-exp? exps)
+				 (eval (first-exp exps) env))
+				(else
+					(eval (first-exp exps) env)
+					(eval-sequence (rest-exps exp) env))))
+
+
+(define (eval-assignment exp env)
+	(set-variable-value! (assignment-variable exp)
+											 (eval (assignment-value exp) env)
+											 env)
+	'ok)
+
+
+(define (eval-definition exp env)
+	(define-variable! (definition-variable exp)
+										(eval (definition-value exp) env)
+										env)
+	'ok)
