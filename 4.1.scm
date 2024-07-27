5a77476ca48826594ab327cc381a6125181f065e
M. Taimoor Zaeem <mtaimoorzaeem@gmail.com>
2024-07-27 17:10:49 +0500

    Add Ex 4.1

--- 4.1.scm
+++ 4.1.scm
@@ -66,3 +66,23 @@
 										(eval (definition-value exp) env)
 										env)
 	'ok)
+
+
+;; Ex 4.1
+;; ================
+
+(define (list-of-values-l2r exps env)
+	(if (no-operands? exps)
+			'()
+			(let ((first-exp (eval (first-operand exps) env))
+					(cons first-exp
+								(list-of-values-l2r (rest-operands exps) env))))))
+
+
+(define (list-of-values-r2l exps env)
+		(list-of-values-l2r (reverse exps) env))
+
+
+
+
+
