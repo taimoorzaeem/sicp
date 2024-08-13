2610a4c6391abe6ff353e2cc7da370234dcb2625
M. Taimoor Zaeem <mtaimoorzaeem@gmail.com>
2024-08-13 19:39:20 +0500

    Add Ex 3.56

--- 3.5.scm
+++ 3.5.scm
@@ -135,3 +135,37 @@
 ;; (stream-ref (partial-sums integers) 1) ==> 3
 ;; (stream-ref (partial-sums integers) 2) ==> 6
 ;; (stream-ref (partial-sums integers) 3) ==> 10
+
+
+;; Ex 3.56
+;; =============
+
+(define (scale-stream stream factor)
+  (stream-map (lambda (x) (* x factor))
+              stream))
+
+(define (merge s1 s2)
+  (cond ((stream-null? s1) s2)
+        ((stream-null? s2) s1)
+        (else
+        (let ((s1car (stream-car s1))
+              (s2car (stream-car s2)))
+          (cond ((< s1car s2car)
+                 (cons-stream
+                  s1car
+                  (merge (stream-cdr s1) s2)))
+                ((> s1car s2car)
+                 (cons-stream
+                  s2car
+                  (merge s1 (stream-cdr s2))))
+                (else
+                  (cons-stream
+                   s1car
+                   (merge (stream-cdr s1)
+                          (stream-cdr s2)))))))))
+
+(define S 
+  (cons-stream 1 
+               (merge (scale-stream S 2)
+                      (merge (scale-stream S 3) 
+                             (scale-stream S 5)))))
