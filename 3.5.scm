0014694cb46af5328a21d42a8b377e737eca26f6
M. Taimoor Zaeem <mtaimoorzaeem@gmail.com>
2024-08-13 13:31:28 +0500

    Add Ex 3.54

--- 3.5.scm
+++ 3.5.scm
@@ -96,3 +96,26 @@
 ;; ======
 ;;
 ;; s == (1 2 4 8 16 32 ...)
+
+
+;; Ex 3.54
+;; ============
+
+(define ones (cons-stream 1 ones))
+
+(define integers (cons-stream 1 (add-streams ones integers)))
+
+(define (mul-streams s1 s2) (stream-map * s1 s2))
+
+(define factorials
+  (cons-stream 1 
+               (mul-streams (stream-cdr integers) 
+                            factorials)))
+
+;; Output
+;;
+;; (stream-ref factorials 0) ==> 1
+;; (stream-ref factorials 2) ==> 6
+;; (stream-ref factorials 3) ==> 24
+;; (stream-ref factorials 5) ==> 720
+
