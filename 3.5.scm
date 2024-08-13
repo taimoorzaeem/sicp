62368c88a85abee4263ccb55829876f555ec6ffe
M. Taimoor Zaeem <mtaimoorzaeem@gmail.com>
2024-08-13 12:07:09 +0500

    Add Ex 3.53

--- 3.5.scm
+++ 3.5.scm
@@ -83,3 +83,16 @@
 ;;  120
 ;;  190
 ;;  210
+
+
+;; Ex 3.53
+;; =============
+
+(define (add-streams s1 s2) (stream-map + s1 s2))
+
+(define s (cons-stream 1 (add-streams s s)))
+
+;; Output
+;; ======
+;;
+;; s == (1 2 4 8 16 32 ...)
