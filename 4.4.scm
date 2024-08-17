bf9c86d6c404510267c66e38e7ba7f098b682d9b
M. Taimoor Zaeem <mtaimoorzaeem@gmail.com>
2024-08-17 11:52:13 +0500

    Add Ex 4.56

--- 4.4.scm
+++ 4.4.scm
@@ -11,3 +11,19 @@
 ;; (supervisor ?name (Bitdiddle Ben))
 ;; (job ?name (accounting . ?type))
 ;; (address ?name (Slumerville . ?street-address))
+
+
+
+;; Ex 4.56
+;; =============
+
+;; Queries:
+;; =======
+;; (and (supervisor ?name (Bitdiddle Ben))
+;;      (address ?name ?address))
+;; (and (salary (Bitdiddle Ben) ?ben-salary)
+;;      (salary ?name ?salary)
+;;      (lisp-value < ?salary ?ben-salary))
+;; (and (supervisor ?name ?supervisor)
+;;      (job ?supervisor ?supervisor-job)
+;;      (not (job ?supervisor (computer . ?title)))))
