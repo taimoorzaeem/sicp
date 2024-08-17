6c71c5f3dca54782cb56e09b3873e7db1e9d7a3a
M. Taimoor Zaeem <mtaimoorzaeem@gmail.com>
2024-08-17 11:31:28 +0500

    Add Ex 4.55

---
+++ 4.4.scm
@@ -0,0 +1,13 @@
+;; SECTION 4.4
+;; ================
+;; LOGIC PROGRAMMING
+
+
+;; Ex 4.55
+;; =============
+
+;; Queries:
+;; =======
+;; (supervisor ?name (Bitdiddle Ben))
+;; (job ?name (accounting . ?type))
+;; (address ?name (Slumerville . ?street-address))
