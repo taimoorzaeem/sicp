254f5080f7103faba88763f67cc9a7010283c515
M. Taimoor Zaeem <mtaimoorzaeem@gmail.com>
2024-08-29 11:02:54 +0500

    Add Ex 5.8

--- 5.2.scm
+++ 5.2.scm
@@ -167,3 +167,25 @@
         (cdr val)
         (error "Undefined label: ASSEMBLE"
                label-name))))
+
+
+;; Ex 5.8
+;; ============
+
+(define (extract-labels text receive) 
+   (if (null? text) 
+       (receive '() '()) 
+       (extract-labels 
+        (cdr text) 
+          (lambda (insts labels) 
+            (let ((next-inst (car text))) 
+              (if (symbol? next-inst) 
+                (if (assoc next-inst labels) 
+                    (error "Repeated label name" next-inst) 
+                    (receive insts 
+                             (cons (make-label-entry next-inst 
+                                                     insts) 
+                                   labels)))) 
+                    (receive (cons (make-instruction next-inst) 
+                                   insts)
+                             labels))))))
