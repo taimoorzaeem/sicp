d6742bd714046cb83a0410974dc425f44bebadc1
M. Taimoor Zaeem <mtaimoorzaeem@gmail.com>
2024-08-31 14:03:20 +0500

    Add Ex 5.16

--- 5.2.scm
+++ 5.2.scm
@@ -399,3 +399,68 @@
 ;;          (begin
 ;;            (allocate-register name)
 ;;            (lookup-register name)))))
+
+
+
+;; Ex 5.16
+;; ============
+
+
+(define (make-new-machine)
+  (let ((pc (make-register 'pc))
+        (flag (make-register 'flag))
+        (stack (make-stack))
+        (the-instruction-sequence '())
+        (trace-on false))
+    (let ((the-ops
+           (list (list 'initialize-stack
+                       (lambda () (stack 'initialize)))))
+           (register-table
+            (list (list 'pc pc) (list 'flag flag))))
+      (define (allocate-register name)
+        (if (assoc name register-table)
+            (error "Multiply defined register: " name)
+            (set! register-table
+                  (cons (list name (make-register name))
+                        register-table)))
+        'register-allocated)
+      (define (lookup-register name)
+        (let ((val (assoc name register-table)))
+          (if val
+              (cadr val)
+              (error "Unknown register:" name))))
+      (define (execute)
+        (let ((insts (get-contents pc)))
+          (if (null? insts)
+              'done
+              (begin
+                (if trace-on
+                  (begin (display (caar insts)) (newline)))
+                ((instruction-execution-proc (car insts)))
+                (execute)))))
+      (define (dispatch message)
+        (cond ((eq? message 'start)
+               (set-contents! pc the-instruction-sequence)
+               (execute))
+              ((eq? message 'install-instruction-sequence)
+               (lambda (seq)
+                (set! the-instruction-sequence seq)))
+              ((eq? message 'allocate-register)
+               allocate-register)
+              ((eq? message 'trace-on) (set! trace-on true))
+              ((eq? message 'trace-off) (set! trace-on false))
+              ((eq? message 'get-register)
+               lookup-register)
+              ((eq? message 'install-operations)
+               (lambda (ops)
+                (set! the-ops (append the-ops ops))))
+              ((eq? message 'stack) stack)
+              ((eq? message 'operations) the-ops)
+              (else (error "Unknown request: MACHINE"
+                           message))))
+        dispatch)))
+
+(define (trace-on-instruction machine)
+  (machine 'trace-on))
+(define (trace-off-instruction machine)
+  (machine 'trace-off))
