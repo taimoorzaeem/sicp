2224a11395a9dd759258b6fd874fa22902d3cf6f
M. Taimoor Zaeem <mtaimoorzaeem@gmail.com>
2024-09-01 16:54:50 +0500

    Add Driver Loop code

--- 4.4.scm
+++ 4.4.scm
@@ -127,3 +127,48 @@
 ;; (rule (reverse (?h . ?t) ?y)
 ;;       (and (reverse ?t ?reversed-t)
 ;;            (append-to-form ?reversed-t (?h) ?y)))
+
+
+
+;; Driver Loop Code
+
+(define input-prompt ";;; Query input:")
+(define output-prompt ";;; Query results:")
+
+
+(define (query-driver-loop)
+  (prompt-for-input input-prompt)
+  (let ((q (query-syntax-process (read))))
+    (cond ((assertion-to-be-added? q)
+           (add-rule-or-assertion! (add-assertion-body q))
+           (newline)
+           (display "Assertion added to data base.")
+           (query-driver-loop))
+          (else
+           (newline)
+           (display output-prompt)
+           (display-stream
+            (stream-map
+             (lambda (frame)
+              (instantiate
+              q
+              frame
+              (lambda (v f)
+                (contract-question-mark v))))
+            (qeval q (singleton-stream '()))))
+          (query-driver-loop)))))
+
+
+(define (instantiate exp frame unbound-var-handler)
+  (define (copy exp)
+    (cond ((var? exp)
+           (let ((binding (binding-in-frame exp frame)))
+             (if binding
+                 (copy (binding-value binding))
+                 (unbound-var-handler exp frame))))
+          ((pair? exp)
+           (cons (copy (car exp)) (copy (cdr exp))))
+          (else exp)))
+  (copy exp))
+
+
