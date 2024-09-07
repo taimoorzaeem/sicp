cc011ffe315218869470a3335c2f02e2e8bd8b8d
M. Taimoor Zaeem <mtaimoorzaeem@gmail.com>
2024-09-07 19:47:57 +0500

    Add code for evaluating compound queries

--- 4.4.scm
+++ 4.4.scm
@@ -186,3 +186,18 @@
        (find-assertions query-pattern frame)
        (delay (apply-rules query-pattern frame))))
     frame-stream))
+
+
+(define (conjoin conjuncts frame-stream)
+  (if (empty-conjunction? conjuncts)
+      frame-stream
+      (conjoin (rest-conjuncts conjuncts)
+               (qeval (first-conjunct conjuncts) frame-stream))))
+
+
+(define (disjoin disjuncts frame-stream)
+  (if (empty-disjunction? disjuncts)
+      the-empty-stream
+      (interleave-delayed
+       (qeval (first-disjunct disjuncts) frame-stream)
+       (delay (disjoin (rest-disjuncts disjuncts) frame-stream)))))
